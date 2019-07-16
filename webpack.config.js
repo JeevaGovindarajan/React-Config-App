dec6ba19a882f4ec9021a982098415bf7c55c22c
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2019-07-17 00:59:04 +0530

    modified the entry and template path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    entry: "index.js",
+    entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
@@ -16,7 +16,7 @@ const config = {
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: './src/index.html'
         })
     ]
 }
