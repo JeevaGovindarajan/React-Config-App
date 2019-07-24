662de2b43617dc306b4d713948751a524b4ac266
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2019-07-25 02:10:48 +0530

    modified to library import fontawesome

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './Components/App/App';
 import './Components/App/App.css';
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(fab, fas);
 
 render(<App />, document.getElementById('app'));
