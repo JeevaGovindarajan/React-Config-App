18db0ad1cecc45b24e11a08847606ddba5cfe024
JeevaGovindarajan <jeeva.govindarajan@anywhere.co>
2019-11-14 15:10:19 +0530

    configured typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
+    // entry: ["react-hot-loader/patch", "./src/index.js"],  for hot reloading
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    devtool: "inline-source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
     module: {
         rules: [
+            {test: /\.tsx?$/, exclude: "/node_modules/" , use: "ts-loader"},
             {test: /\.(js)$/, exclude: "/node_modules/", use: 'babel-loader'},
-            {test: /\.css$/, use: ['style-loader', 'css-loader']}
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
         ]
     },
     mode: 'development',
